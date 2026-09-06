722353a9b38fe9ddc035258f2da4362c25436710
Tomáš Vojík <vojik@wboy.cz>
2026-09-06 11:34:58 +0200

    :art: align factory interface with package coding standard

--- src/Factory/InertiaFactoryInterface.php
+++ src/Factory/InertiaFactoryInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Lsr\Inertia\Factory;
@@ -8,7 +9,5 @@ use Psr\Http\Message\ServerRequestInterface;
 
 interface InertiaFactoryInterface
 {
-
     public function fromRequest(ServerRequestInterface $request): Inertia;
-
 }
